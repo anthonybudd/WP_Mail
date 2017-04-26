3e72337eed6f3a82628fdaf6891cef15b300a2e5
Anthony Budd <anthonybudd94@gmail.com>
2017-04-26 15:09:18 +0100

    Init

---
+++ src/WP_Mail.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * WP_Mail
+ *
+ * A simple class for creating and
+ * sending Emails using WordPress
+ *
+ * @author     AnthonyBudd <anthonybudd94@gmail.com>
+ */
+Abstract Class WP_Mail
+{
+
+}
